25163789a8c309e7a4b5d5f9c2f3b9acbfb1937d
Irfan Khan <irfankhan501@outlook.com>
2020-12-05 15:53:34 +0530

    answer

--- src/app.js
+++ src/app.js
@@ -1,48 +1,29 @@
-
-const express = require('express');
-const Subscriber = require("./models/subscribers")
-const app = express()
-
+const express = require("express");
+const Subscriber = require("./models/subscribers");
+const app = express();
 
 // Your code goes here
-app.get("/subscribers", async(req, res) => {
-    const result = await Subscriber.find()
-    res.send(result)
-})
+app.get("/subscribers", async (req, res) => {
+  const result = await Subscriber.find();
+  res.send(result);
+});
 
-app.get("/subscribers/names", async(req, res) => {
-    const result = await Subscriber.find().select({ name:1, subscribedChannel:1, _id: 0})
-    res.send(result)
-})
+app.get("/subscribers/names", async (req, res) => {
+  const result = await Subscriber.find().select({
+    name: 1,
+    subscribedChannel: 1,
+    _id: 0,
+  });
+  res.send(result);
+});
 
-app.get("/subscribers/:id", async(req, res) => {
+app.get("/subscribers/:id", async (req, res) => {
+  try {
     const result = await Subscriber.findById(req.params.id);
-    if (!result)
-      return res.status(400).send({message: result.message});
-  
     res.send(result);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
+  }
+});
 
 module.exports = app;
