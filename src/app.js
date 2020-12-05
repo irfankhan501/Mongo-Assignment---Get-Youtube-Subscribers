c119286060aebf541570efe21dbba55d15fe6719
Irfan Khan <irfankhan501@outlook.com>
2020-12-05 15:49:12 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 
 const express = require('express');
+const Subscriber = require("./models/subscribers")
 const app = express()
 
 
 // Your code goes here
+app.get("/subscribers", async(req, res) => {
+    const result = await Subscriber.find()
+    res.send(result)
+})
+
+app.get("/subscribers/names", async(req, res) => {
+    const result = await Subscriber.find().select({ name:1, subscribedChannel:1, _id: 0})
+    res.send(result)
+})
+
+app.get("/subscribers/:id", async(req, res) => {
+    const result = await Subscriber.findById(req.params.id);
+    if (!result)
+      return res.status(400).send({message: result.message});
+  
+    res.send(result);
+})
+
+
 
 
 
